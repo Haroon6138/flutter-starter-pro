0b05596b0e3615aa7902bbcb031e83e88a6a584f
Haroon6138 <47740408+Haroon6138@users.noreply.github.com>
2025-08-29 17:36:02 +0300

    Add route generator

---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../presentation/screens/splash/splash_screen.dart';
+import '../presentation/screens/auth/login_screen.dart';
+import '../presentation/screens/auth/register_screen.dart';
+import '../presentation/screens/home/home_screen.dart';
+import '../presentation/screens/profile/profile_screen.dart';
+import '../presentation/screens/settings/settings_screen.dart';
+import 'app_routes.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case AppRoutes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case AppRoutes.register:
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+      case AppRoutes.home:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case AppRoutes.profile:
+        return MaterialPageRoute(builder: (_) => const ProfileScreen());
+      case AppRoutes.settings:
+        return MaterialPageRoute(builder: (_) => const SettingsScreen());
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child: Text('No route defined for ${settings.name}'),
+                  ),
+                ));
+    }
+  }
+}
